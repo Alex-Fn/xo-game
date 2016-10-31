d40d94fc0a1d2994d558377abcaec18d44af9191
Alex-Fn <regalex82@mail.ru>
2016-10-31 17:25:03 +0600

    AlreadyOccupiedException  Test added

--- tst/io/game/xo/model/FieldTest.java
+++ tst/io/game/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.game.xo.model;
 
+import io.game.xo.model.exceptions.AlreadyOccupiedException;
 import io.game.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -27,6 +28,21 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
+
+    }
+
+    @Test
+    public void setFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
 
     }
 
