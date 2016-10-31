fb132f4725540ffa3f34f61498bacd6251889cd7
Alex-Fn <regalex82@mail.ru>
2016-10-31 17:02:48 +0600

    Field - tests for the Point added

--- src/io/game/xo/model/Field.java
+++ src/io/game/xo/model/Field.java
@@ -1,11 +1,18 @@
 package io.game.xo.model;
 
 
+import io.game.xo.model.exceptions.AlreadyOccupiedException;
+import io.game.xo.model.exceptions.InvalidePointException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+
+    public static final int MIN_COORDINATE = 0;
+
+    public static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -13,12 +20,30 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidePointException {
+        if (!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidePointException,
+                                                                         AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordiante(point.x) && checkCoordiante(point.y);
+    }
+
+    private boolean checkCoordiante(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 
 }
